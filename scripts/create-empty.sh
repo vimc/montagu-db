bfd551a04ef557af360b170bea1156bf80027e02
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-03-15 14:39:23 +0000

    Clean up docker containers on failure

--- scripts/create-empty.sh
+++ scripts/create-empty.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-set -e
+
+# To debug:
+# set -x
 
 IMAGE=vimc/montagu-db
 DATA_VOLUME=$1
@@ -11,8 +13,8 @@ fi
 
 if [[ -z "${DATA_VOLUME}" ]]; then
     echo "Creating new unnamed data volume"
-    DOCKER_VOLUME=$(docker volume create)
-    echo "Volume name: ${DOCKER_VOLUME}"
+    DATA_VOLUME=$(docker volume create)
+    echo "Volume name: ${DATA_VOLUME}"
 else
     echo "Creating new data volume '${DATA_VOLUME}'"
     docker volume create ${DATA_VOLUME}
@@ -25,9 +27,19 @@ else
     echo "Image '${IMAGE}' exists"
 fi
 
-BUILD_ID=$(docker run -d -v $DATA_VOLUME:/var/lib/postgresql/data $IMAGE)
+BUILD_ID=$(docker run -d \
+                  -v ${DATA_VOLUME}:/var/lib/postgresql/data \
+                  -v ${PWD}/schema:/montagu-schema \
+                  $IMAGE)
 docker exec -it $BUILD_ID montagu-create-schema.sh
+SUCCESS=$?
 docker stop $BUILD_ID
 docker rm $BUILD_ID
 
-echo "Created new data volume '$DATA_VOLUME'"
+if [[ $SUCCESS -eq 0 ]]; then
+    echo "Created new data volume '$DATA_VOLUME'"
+    exit 0
+else
+    echo "Build failed"
+    exit 1
+fi
