fd96fb03a241cefa2c6303b9191b1a92cb74e791
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-04-10 15:30:52 +0100

    Script to do the import

---
+++ import/scripts/import-and-dump.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Expected one argument (the import path)"
+fi
+MONTAGU_IMPORT_PATH=$1
+
+if [ ! -d $MONTAGU_IMPORT_PATH ]; then
+    echo "Import path must exist and be a directory"
+    exit 1
+fi
+MONTAGU_IMPORT_PATH_ABS=$(readlink -f $MONTAGU_IMPORT_PATH)
+
+MONTAGU_IMPORT_PATH_CONTAINER=/montagu-import-data
+
+MONTAGU_DB_HOST=montagu-db-server
+MONTAGU_DB_TAG=master
+MONTAGU_DB_IMAGE=montagu.dide.ic.ac.uk:5000/montagu-db:${MONTAGU_DB_TAG}
+
+MONTAGU_DB_NETWORK=montagu-db-import-nw
+
+NETWORK=$(docker network create $MONTAGU_DB_NETWORK)
+docker run -d --rm \
+       --name $MONTAGU_DB_HOST \
+       --network $MONTAGU_DB_NETWORK \
+       $MONTAGU_DB_IMAGE
+
+# First wait for the container to come up; for now this should be enough
+sleep 1
+
+docker run --rm -it \
+       --network $MONTAGU_DB_NETWORK \
+       -e MONTAGU_DB_HOST=$MONTAGU_DB_HOST \
+       -e MONTAGU_DB_PORT=5432 \
+       -e MONTAGU_IMPORT_PATH=$MONTAGU_IMPORT_PATH_CONTAINER \
+       -v $MONTAGU_IMPORT_PATH_ABS:$MONTAGU_IMPORT_PATH_CONTAINER \
+       montagu.dide.ic.ac.uk:5000/montagu-db-import:master bash
+
+SUCCESS=$?
+
+if [ $SUCCESS -eq 0 ]; then
+    echo "Success!"
+    pg_dump -U vimc -Fc montagu > montagu.dump
+fi
+
+docker stop $MONTAGU_DB_HOST
+docker network rm $MONTAGU_DB_NETWORK
+
+exit $SUCCESS
