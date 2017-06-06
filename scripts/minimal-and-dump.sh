ae8b599fbc2854dddde5a4c453d935647828d1ed
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-06-06 09:56:14 +0100

    Use correct path to import script

--- scripts/minimal-and-dump.sh
+++ scripts/minimal-and-dump.sh
@@ -3,4 +3,4 @@ set -x
 MONTAGU_DB_HASH=$(git rev-parse --short HEAD)
 REGISTRY=montagu.dide.ic.ac.uk:5000
 docker pull $REGISTRY/montagu-db:$MONTAGU_DB_HASH
-./montagu-db/import/scripts/import-and-dump.sh $MONTAGU_DB_HASH
+./import/scripts/import-and-dump.sh $MONTAGU_DB_HASH
