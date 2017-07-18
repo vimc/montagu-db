e6981c3d223bcb5a1a3e8bb6dec689926de0e16a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-07-18 13:44:19 +0100

    More indexes

--- functions/indexes.sql
+++ functions/indexes.sql
@@ -3,3 +3,12 @@ CREATE UNIQUE INDEX idx_user_username_unique
 
 CREATE UNIQUE INDEX idx_user_email_unique 
     ON app_user (lower(email));
+
+-- The demographic_statistic table gets very large so we need to index
+-- a few things to make it possible to index efficiently.
+-- Unfortunately this also makes inserts very slow!
+CREATE INDEX ON demographic_statistic (projection_variant);
+CREATE INDEX ON demographic_statistic (gender);
+CREATE INDEX ON demographic_statistic (country);
+CREATE INDEX ON demographic_statistic (demographic_statistic_type);
+CREATE INDEX ON demographic_statistic (source);
