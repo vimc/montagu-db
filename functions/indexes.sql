08ce286ebc841c7c1f62992a305c2ecfcd71451f
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-07-21 11:25:14 +0100

    Fix index script

--- functions/indexes.sql
+++ functions/indexes.sql
@@ -7,7 +7,7 @@ CREATE UNIQUE INDEX idx_user_email_unique
 -- The demographic_statistic table gets very large so we need to index
 -- a few things to make it possible to index efficiently.
 -- Unfortunately this also makes inserts very slow!
-CREATE INDEX ON demographic_statistic (projection_variant);
+CREATE INDEX ON demographic_statistic (demographic_variant);
 CREATE INDEX ON demographic_statistic (gender);
 CREATE INDEX ON demographic_statistic (country);
 CREATE INDEX ON demographic_statistic (demographic_statistic_type);
