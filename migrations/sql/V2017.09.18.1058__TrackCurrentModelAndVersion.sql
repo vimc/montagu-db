7790bcf61444f35143ea2de66f7b761c1ada6f62
Martin Eden <martin.s.eden@gmail.com>
2017-09-18 15:13:09 +0100

    Make case consistent.

--- migrations/sql/V2017.09.18.1058__TrackCurrentModelAndVersion.sql
+++ migrations/sql/V2017.09.18.1058__TrackCurrentModelAndVersion.sql
@@ -15,36 +15,36 @@ ALTER TABLE model
 -- Add disease information to models
 UPDATE model SET disease = 'HepB' WHERE id in 
     ('HepBGoldstein', 'HepBIC', 'HepBSTATIC', 'PRoGReSs');
-UPDATE model SET disease = 'Hib' where id in
+UPDATE model SET disease = 'Hib' WHERE id in
     ('LiST-Hib', 'TRIVAC-Hib');
-UPDATE model SET disease = 'HPV' where id in
+UPDATE model SET disease = 'HPV' WHERE id in
     ('PRIME', 'HPVGoldie', 'HPVGoldie-flat', 'HPVGoldie-linear');
-UPDATE model SET disease = 'JE' where id in 
+UPDATE model SET disease = 'JE' WHERE id in 
     ('JE-Clapham', 'PATH-JE');
-UPDATE model SET disease = 'Measles' where id in 
+UPDATE model SET disease = 'Measles' WHERE id in 
     ('DynaMICE', 'MeaslesPSU');
-UPDATE model SET disease = 'MenA' where id in
+UPDATE model SET disease = 'MenA' WHERE id in
     ('MenA-Cambridge', 'MenAJackson', 'PATH-MenA');
-UPDATE model SET disease = 'PCV' where id in
+UPDATE model SET disease = 'PCV' WHERE id in
     ('LiST-PCV', 'TRIVAC-PCV');
-UPDATE model SET disease = 'Rota' where id in
+UPDATE model SET disease = 'Rota' WHERE id in
     ('LiST-Rota', 'TRIVAC-Rota');
-UPDATE model SET disease = 'Rubella' where id in
+UPDATE model SET disease = 'Rubella' WHERE id in
     ('RubellaPHE');
-UPDATE model SET disease = 'YF' where id in
+UPDATE model SET disease = 'YF' WHERE id in
     ('UnknownYF', 'YFIC');
 
 -- Add is_current information to models
 UPDATE model SET is_current = TRUE;
-UPDATE model SET is_current = FALSE where modelling_group IN 
+UPDATE model SET is_current = FALSE WHERE modelling_group IN 
     ('unknown', 'Harvard-Sweet', 'LSHTM-Edmunds');
-UPDATE model SET is_current = FALSE where model IN 
+UPDATE model SET is_current = FALSE WHERE model IN 
     ('PATH-MenA');
 -- This is the only modelling group that has more than one model for the same
 -- disease. I've just picked one of three models at random - we need Tini on
 -- this one.
 UPDATE model SET is_current = TRUE 
-    where modelling_group = 'Harvard-Sweet'
+    WHERE modelling_group = 'Harvard-Sweet'
     and id = 'HPVGoldie';
 
 -- Create a new version for every model
