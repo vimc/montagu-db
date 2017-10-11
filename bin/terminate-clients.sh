05e42fe6a0ef6059087854ba4aed625d69ea99e5
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-10-11 14:10:40 +0100

    Add attribution for the terminate clients script

--- bin/terminate-clients.sh
+++ bin/terminate-clients.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# Terminate all clients connected to the montagu database except for
+# the calling one:
+#
+# https://stackoverflow.com/a/5408501
 psql -d postgres -U vimc <<EOF
 SELECT pg_terminate_backend(pg_stat_activity.pid)
 FROM pg_stat_activity
