9dc6d0988aa902f3dd97a7a80d045ac0a329b7af
Rich <r.fitzjohn@imperial.ac.uk>
2017-11-09 15:36:36 +0000

    Expand migrations to cover VIMC-973

--- migrations/sql/V2017.11.10.1533__AddGaviSupportToCoverage.sql
+++ migrations/sql/V2017.11.10.1533__AddGaviSupportToCoverage.sql
@@ -2,4 +2,6 @@
 -- per-year GAVI has supported a given vaccination activity.
 ALTER TABLE coverage
     ADD COLUMN gavi_support boolean DEFAULT NULL
+    ADD COLUMN gender integer DEFAULT NULL
+    ADD FOREIGN KEY (gender) REFERENCES gender (id)
     ;
