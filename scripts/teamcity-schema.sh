b945ccf59bdfe4534b2d3ec5b6fe8a6c758c259c
Rich <r.fitzjohn@imperial.ac.uk>
2017-11-13 14:26:34 +0000

    Only apply short argument to rev-parse

--- scripts/teamcity-schema.sh
+++ scripts/teamcity-schema.sh
@@ -2,7 +2,7 @@
 set -e
 
 GIT_ID=$(git rev-parse --short=7 HEAD)
-GIT_BRANCH=$(git symbolic-ref --short=7 HEAD)
+GIT_BRANCH=$(git symbolic-ref --short HEAD)
 REGISTRY=docker.montagu.dide.ic.ac.uk:5000
 NAME=montagu-db
 
