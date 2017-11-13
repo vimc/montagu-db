9f9d66cebed7f783cf0d33e8a316615020e6d6dd
Alex Hill <alex.hill@gmail.com>
2017-11-13 18:34:37 +0000

    burden estimate set status added

---
+++ migrations/sql/V2017.11.13.1828__AddBurdenEstimateSetStatus.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "burden_estimate_set_status" (
+  "code" TEXT NOT NULL ,
+  "description" TEXT NOT NULL ,
+  PRIMARY KEY ("code")
+);
+COMMENT ON COLUMN "burden_estimate_set_status"."code" IS 'Valid values: {empty, partial, complete}';
+
+INSERT INTO burden_estimate_set_status VALUES ('empty', 'No burden estimates added');
+INSERT INTO burden_estimate_set_status VALUES ('partial', 'Some but not all burden estimates added');
+INSERT INTO burden_estimate_set_status VALUES ('complete', 'Closed to further estimates');
