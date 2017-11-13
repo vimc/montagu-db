94e5ec338d656b9ffb53f9c5ab69d9a8abe1d072
Alex Hill <alex.hill@gmail.com>
2017-11-13 18:36:26 +0000

    add status to burden_estimate_set table

--- migrations/sql/V2017.11.13.1828__AddBurdenEstimateSetStatus.sql
+++ migrations/sql/V2017.11.13.1828__AddBurdenEstimateSetStatus.sql
@@ -1,10 +1,14 @@
-CREATE TABLE "burden_estimate_set_status" (
-  "code" TEXT NOT NULL ,
-  "description" TEXT NOT NULL ,
-  PRIMARY KEY ("code")
+CREATE TABLE burden_estimate_set_status (
+  code TEXT NOT NULL ,
+  description TEXT NOT NULL ,
+  PRIMARY KEY (code)
 );
-COMMENT ON COLUMN "burden_estimate_set_status"."code" IS 'Valid values: {empty, partial, complete}';
+COMMENT ON COLUMN burden_estimate_set_status.code IS 'Valid values: {empty, partial, complete}';
 
 INSERT INTO burden_estimate_set_status VALUES ('empty', 'No burden estimates added');
 INSERT INTO burden_estimate_set_status VALUES ('partial', 'Some but not all burden estimates added');
 INSERT INTO burden_estimate_set_status VALUES ('complete', 'Closed to further estimates');
+
+ALTER TABLE burden_estimate_set
+  ADD COLUMN status TEXT DEFAULT 'empty' NULL,
+  ADD FOREIGN KEY ("status") REFERENCES burden_estimate_set_status (code);
