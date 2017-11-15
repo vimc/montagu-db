72bc3fa65eed77fbef15a5cc9870af404adc423f
Alex Hill <alex.hill@gmail.com>
2017-11-15 16:44:51 +0000

    wait in right plac

--- scripts/teamcity-migrate.sh
+++ scripts/teamcity-migrate.sh
@@ -19,10 +19,10 @@ docker build \
 
 docker network create migration_test
 
-docker exec db montagu-wait.sh
-
 docker run --rm --network=migration_test -d --name db $DB
 docker run --rm --network=migration_test $COMMIT_TAG
+
+docker exec db montagu-wait.sh
 
 docker stop db
 docker network rm migration_test
