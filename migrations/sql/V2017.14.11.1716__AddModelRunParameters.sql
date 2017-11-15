1d31e442a4a1966ab9457e4997409bc2728d859f
Alex Hill <alex.hill@gmail.com>
2017-11-15 12:07:54 +0000

    primary keys

--- migrations/sql/V2017.14.11.1716__AddModelRunParameters.sql
+++ migrations/sql/V2017.14.11.1716__AddModelRunParameters.sql
@@ -1,7 +1,8 @@
 CREATE TABLE upload_info(
 id SERIAL,
 uploaded_by TEXT NOT NULL,
-uploaded_on TEXT NOT NULL
+uploaded_on TEXT NOT NULL,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE model_run_parameter_set(
@@ -9,7 +10,8 @@ CREATE TABLE model_run_parameter_set(
    responsibility_set INTEGER NOT NULL,
    description TEXT,
    upload_info INTEGER NOT NULL,
-   model_version INTEGER NOT NULL
+   model_version INTEGER NOT NULL,
+   PRIMARY KEY (id)
 );
 
 ALTER TABLE model_run_parameter_set ADD FOREIGN KEY (responsibility_set) REFERENCES responsibility_set (id);
@@ -19,7 +21,8 @@ ALTER TABLE model_run_parameter_set ADD FOREIGN KEY (upload_info) REFERENCES upl
 CREATE TABLE model_run(
 internal_id SERIAL,
 run_id TEXT NOT NULL,
-model_run_parameter_set INTEGER NOT NULL
+model_run_parameter_set INTEGER NOT NULL,
+PRIMARY KEY (id)
 );
 
 ALTER TABLE model_run ADD FOREIGN KEY (model_run_parameter_set) REFERENCES model_run_parameter_set (id);
@@ -27,7 +30,8 @@ ALTER TABLE model_run ADD FOREIGN KEY (model_run_parameter_set) REFERENCES model
 CREATE TABLE model_run_parameter(
 model_run INTEGER NOT NULL,
 key TEXT NOT NULL,
-value TEXT NOT NULL
+value TEXT NOT NULL,
+UNIQUE (key)
 );
 
 ALTER TABLE model_run_parameter ADD FOREIGN KEY (model_run) REFERENCES model_run (internal_id);
