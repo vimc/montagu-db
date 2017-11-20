02fb63ab5bfb2663cd6e8eb1a191d74329b987de
Martin Eden <martin.s.eden@gmail.com>
2017-11-20 13:40:16 +0000

    Also update default.

--- migrations/sql/V2017.11.20.1226__CorrectBurdenEstimateSetTypeCodes.sql
+++ migrations/sql/V2017.11.20.1226__CorrectBurdenEstimateSetTypeCodes.sql
@@ -6,4 +6,5 @@ UPDATE burden_estimate_set SET set_type = replace(set_type, '_', '-');
 
 ALTER TABLE burden_estimate_set
     ADD CONSTRAINT burden_estimate_set_set_type_fkey
-    FOREIGN KEY (set_type) REFERENCES burden_estimate_set_type(code);
+    FOREIGN KEY (set_type) REFERENCES burden_estimate_set_type(code),
+    ALTER COLUMN set_type SET DEFAULT 'central-unknown';
