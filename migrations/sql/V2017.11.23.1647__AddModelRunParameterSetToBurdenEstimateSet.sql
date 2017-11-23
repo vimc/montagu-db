cba704db0d65d9a25668bafbb03b13f0d029bdd7
Alex Hill <alex.hill@gmail.com>
2017-11-23 16:56:49 +0000

    missing comma

--- migrations/sql/V2017.11.23.1647__AddModelRunParameterSetToBurdenEstimateSet.sql
+++ migrations/sql/V2017.11.23.1647__AddModelRunParameterSetToBurdenEstimateSet.sql
@@ -1,3 +1,3 @@
 ALTER TABLE burden_estimate_set
-   ADD COLUMN model_run_parameter_set INTEGER NULl
+   ADD COLUMN model_run_parameter_set INTEGER NULl,
    ADD FOREIGN KEY (model_run_parameter_set) REFERENCES model_run_parameter_set (id);
