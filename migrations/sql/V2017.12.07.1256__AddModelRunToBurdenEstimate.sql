be05a6c348d8f7c31c5f3f76acd2abc2456c3839
Martin Eden <martin.s.eden@gmail.com>
2017-12-08 10:03:54 +0000

    Alter foreign table.

--- migrations/sql/V2017.12.07.1256__AddModelRunToBurdenEstimate.sql
+++ migrations/sql/V2017.12.07.1256__AddModelRunToBurdenEstimate.sql
@@ -2,3 +2,6 @@ ALTER TABLE burden_estimate
     ADD COLUMN model_run INTEGER NULL,
     ADD FOREIGN KEY (model_run) REFERENCES model_run (internal_id),
     DROP COLUMN stochastic;
+
+ALTER FOREIGN TABLE burden_estimate_stochastic
+    ADD COLUMN model_run INTEGER NOT NULL;
