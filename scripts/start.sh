31b65a97aba136c631dba3f1693e47f0f1f46f26
Alex Hill <alex.hill@gmail.com>
2018-01-09 21:22:47 +0000

    add use mapping

--- scripts/start.sh
+++ scripts/start.sh
@@ -71,4 +71,8 @@ docker exec $DB_ANNEX_CONTAINER montagu-wait.sh
 docker run --rm --network=$NETWORK $MIGRATE_IMAGE -configFile=conf/flyway-annex.conf migrate
 docker run --rm --network=$NETWORK $MIGRATE_IMAGE
 
+# Add user mapping
+docker exec $DB_CONTAINER psql -U vimc -d montagu -c \
+       "CREATE USER MAPPING FOR vimc SERVER montagu_db_annex OPTIONS (user 'vimc', password 'changeme');"
+
 trap - EXIT
