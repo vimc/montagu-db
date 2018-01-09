5618d65839ffd1dd7374c8fb77fcf18203203658
Alex Hill <alex.hill@gmail.com>
2018-01-09 21:38:37 +0000

    change order

--- scripts/start.sh
+++ scripts/start.sh
@@ -67,12 +67,13 @@ docker run --rm --network=$NETWORK -d \
 docker exec $DB_CONTAINER montagu-wait.sh
 docker exec $DB_ANNEX_CONTAINER montagu-wait.sh
 
+# Do the migrations
+docker run --rm --network=$NETWORK $MIGRATE_IMAGE
+
 # Add user mapping
 docker exec $DB_CONTAINER psql -U vimc -d montagu -c \
        "CREATE USER MAPPING FOR vimc SERVER montagu_db_annex OPTIONS (user 'vimc', password 'changeme');"
 
-# Do the migrations
 docker run --rm --network=$NETWORK $MIGRATE_IMAGE -configFile=conf/flyway-annex.conf migrate
-docker run --rm --network=$NETWORK $MIGRATE_IMAGE
 
 trap - EXIT
