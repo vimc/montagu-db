37816366dd5e44ac20803290ca6e9e5233477945
Martin Eden <martin.s.eden@gmail.com>
2018-01-24 17:13:07 +0000

    Fix syntax

--- migrations/sql/V2018.01.24.17.02__AddEstimateWriterRole.sql
+++ migrations/sql/V2018.01.24.17.02__AddEstimateWriterRole.sql
@@ -7,7 +7,7 @@ DO $$
 		null, 
 		'Upload burden estimates to ANY group (useful for stochastic ingest tool)'
 	)
-	returning id into role_id;
+	returning id as role_id;
 
 	insert into role_permission values (role_id, 'estimates.read-unfinished');
 	insert into role_permission values (role_id, 'estimates.write');
