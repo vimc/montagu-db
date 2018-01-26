fde7f2b24c940701064eb8126a48a44dd290966e
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-01-26 10:05:54 +0000

    Create tables to hold new gavi metadata

---
+++ migrations/sql/V2018.01.26.1000__AddGaviCountryStatus.sql
@@ -0,0 +1,24 @@
+CREATE TABLE "gavi_eligibility_status" (
+  "id" INTEGER,
+  "name" TEXT,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "gavi_eligibility" (
+  "id"  SERIAL,
+  "touchstone" TEXT,
+  "country" TEXT,
+  "year" INTEGER,
+  "status" INTEGER,
+  PRIMARY KEY ("id"),
+  FOREIGN KEY (touchstone) REFERENCES touchstone(id),
+  FOREIGN KEY (country) REFERENCES country(id),
+  FOREIGN KEY (status) REFERENCES gavi_eligibility_status(id)
+);
+
+INSERT INTO gavi_eligibility_status (id, name) VALUES
+  (0, 'Pre-Gavi eligible'),
+  (1, 'Currently eligible'),
+  (2, 'Transitioned'),
+  (3, 'Never eligible - MIC'),
+  (4, 'Never eligible - HIC');
