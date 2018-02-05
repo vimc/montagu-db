a01142ee529184c644f59b31cf9b83ec73ea2370
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-02-05 16:17:55 +0000

    Take two passwords when enabling replication

--- bin/enable-replication.sh
+++ bin/enable-replication.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -e
-PASSWORD=$1
+
+if [ "$#" -ne 2 ]; then
+    echo "Expected two parameters"
+    exit 1
+fi
+PASSWORD_BARMAN=$1
+PASSWORD_STREAM=$2
+
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c \
-    "ALTER ROLE barman WITH PASSWORD '$PASSWORD'"
+    "ALTER ROLE barman WITH PASSWORD '$PASSWORD_BARMAN'"
 psql -U $POSTGRES_USER -d $POSTGRES_DB -c \
-    "ALTER ROLE streaming_barman WITH PASSWORD '$PASSWORD'"
+    "ALTER ROLE streaming_barman WITH PASSWORD '$PASSWORD_STREAM'"
 
 # Replication slot too, ensuring that it is not recreated if this is rerun
 psql -v ON_ERROR_STOP=1 -U vimc -d postgres <<EOF
