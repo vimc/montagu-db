d1ccbdbf45d5b720f85f456c68799fad5b59815e
Alex Hill <alex.hill@gmail.com>
2018-02-15 12:24:45 +0000

    try again

--- migrations/sql/V2018.02.15.1209__DropIntBurdenEstimateColumns.sql
+++ migrations/sql/V2018.02.15.1209__DropIntBurdenEstimateColumns.sql
@@ -6,8 +6,8 @@ ALTER TABLE burden_estimate
   DROP COLUMN burden_outcome;
 
 ALTER TABLE burden_estimate
-  RENAME COLUMN year_small to year,
-  RENAME COLUMN burden_outcome_small to burden_outcome;
+  RENAME COLUMN year_small to 'year',
+  RENAME COLUMN burden_outcome_small to 'burden_outcome';
 
 ALTER TABLE burden_estimate
   ENABLE TRIGGER ALL;
