2e4732027c24b2bffb40894847beadca4e7fcb67
Alex Hill <alex.hill@gmail.com>
2018-02-20 09:17:17 +0000

    use old country index

--- migrations/sql-annex/V2018.02.15.1500__RecreateBurdenEstimateStochasticWithSmallIntFields.sql
+++ migrations/sql-annex/V2018.02.15.1500__RecreateBurdenEstimateStochasticWithSmallIntFields.sql
@@ -4,7 +4,7 @@ CREATE TABLE burden_estimate_stochastic (
   id BIGSERIAL NOT NULL,
   burden_estimate_set integer NOT NULL,
   country text NOT NULL,
-  country_nid SMALLINT NOT NULL,
+  country_nid SMALLINT NULL,
   year SMALLINT NOT NULL,
   burden_outcome SMALLINT NOT NULL,
   stochastic boolean NOT NULL,
@@ -18,7 +18,7 @@ CREATE INDEX ON burden_estimate_stochastic (burden_estimate_set);
 ALTER TABLE burden_estimate_stochastic
   ADD CONSTRAINT burden_estimate_stochastic_unique UNIQUE (
   burden_estimate_set,
-  country_nid,
+  country,
   year,
   age,
   burden_outcome) DEFERRABLE INITIALLY DEFERRED;
