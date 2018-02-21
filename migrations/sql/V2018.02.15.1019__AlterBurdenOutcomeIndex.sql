c479ed097c322249150c8c8c91216d71e9314e05
Alex Hill <alex.hill@gmail.com>
2018-02-21 18:51:29 +0000

    wip

--- migrations/sql/V2018.02.15.1019__AlterBurdenOutcomeIndex.sql
+++ migrations/sql/V2018.02.15.1019__AlterBurdenOutcomeIndex.sql
@@ -1,4 +1,4 @@
--- this took 16 minutes
+-- -- this took 16 minutes
 -- -- disable triggers
 -- ALTER TABLE burden_estimate DISABLE TRIGGER ALL;
 -- ALTER TABLE impact_estimate_ingredient DISABLE TRIGGER ALL;
@@ -15,50 +15,46 @@
 -- ALTER TABLE burden_estimate
 --   ALTER COLUMN burden_outcome TYPE SMALLINT;
 --
--- -- renable triggers
+-- -- reenable triggers
 -- ALTER TABLE burden_estimate ENABLE TRIGGER ALL;
 -- ALTER TABLE impact_estimate_ingredient ENABLE TRIGGER ALL;
 -- ALTER TABLE burden_outcome ENABLE TRIGGER ALL;
 
-
+-- this one loses - 41 minutes!
 -- disable triggers
-ALTER TABLE burden_estimate DISABLE TRIGGER ALL;
-ALTER TABLE impact_estimate_ingredient DISABLE TRIGGER ALL;
-ALTER TABLE burden_outcome DISABLE TRIGGER ALL;
-
--- drop primary key and alter type of burden_outcome.id
-ALTER TABLE burden_outcome
-  ALTER COLUMN id TYPE SMALLINT;
-
--- create new small typed columns
-ALTER TABLE impact_estimate_ingredient
-ADD COLUMN burden_outcome_small SMALLINT;
-
-ALTER TABLE burden_estimate
-  ADD COLUMN burden_outcome_small SMALLINT;
-
--- update new columns
-  UPDATE impact_estimate_ingredient
-SET burden_outcome_small = burden_outcome;
-
-  UPDATE burden_estimate
-SET burden_outcome_small = burden_outcome;
-
--- drop large columns
-ALTER TABLE impact_estimate_ingredient
-DROP COLUMN burden_outcome;
-
-ALTER TABLE burden_estimate
-  DROP COLUMN burden_outcome;
-
--- rename small columns
-ALTER TABLE burden_estimate
-  RENAME COLUMN burden_outcome_small to burden_outcome;
-
-  ALTER TABLE impact_estimate_ingredient
-RENAME COLUMN burden_outcome_small to burden_outcome;
-
--- renable triggers
-ALTER TABLE burden_estimate ENABLE TRIGGER ALL;
-ALTER TABLE impact_estimate_ingredient ENABLE TRIGGER ALL;
-ALTER TABLE burden_outcome ENABLE TRIGGER ALL;
+-- ALTER TABLE burden_estimate DISABLE TRIGGER ALL;
+-- ALTER TABLE impact_estimate_ingredient DISABLE TRIGGER ALL;
+-- ALTER TABLE burden_outcome DISABLE TRIGGER ALL;
+--
+-- -- create new small typed columns
+-- ALTER TABLE impact_estimate_ingredient
+-- ADD COLUMN burden_outcome_small SMALLINT;
+--
+-- ALTER TABLE burden_estimate
+--   ADD COLUMN burden_outcome_small SMALLINT;
+--
+-- -- update new columns
+--   UPDATE impact_estimate_ingredient
+-- SET burden_outcome_small = burden_outcome;
+--
+--   UPDATE burden_estimate
+-- SET burden_outcome_small = burden_outcome;
+--
+-- -- drop large columns
+-- ALTER TABLE impact_estimate_ingredient
+-- DROP COLUMN burden_outcome;
+--
+-- ALTER TABLE burden_estimate
+--   DROP COLUMN burden_outcome;
+--
+-- -- rename small columns
+-- ALTER TABLE burden_estimate
+--   RENAME COLUMN burden_outcome_small to burden_outcome;
+--
+--   ALTER TABLE impact_estimate_ingredient
+-- RENAME COLUMN burden_outcome_small to burden_outcome;
+--
+-- -- renable triggers
+-- ALTER TABLE burden_estimate ENABLE TRIGGER ALL;
+-- ALTER TABLE impact_estimate_ingredient ENABLE TRIGGER ALL;
+-- ALTER TABLE burden_outcome ENABLE TRIGGER ALL;
--- migrations/sql/V2018.02.15.1029__ChangeBurdenEstimateColumnDataTypes.sql
+++ migrations/sql/V2018.02.15.1029__ChangeBurdenEstimateColumnDataTypes.sql
@@ -1,45 +1,21 @@
--- drop all constraints on the table
-ALTER TABLE burden_estimate
-  DROP CONSTRAINT burden_estimate_unique,
-  DROP CONSTRAINT burden_estimate_burden_estimate_set_fkey,
-  DROP CONSTRAINT burden_estimate_country_fkey,
-  DROP CONSTRAINT burden_estimate_pkey,
-  DROP CONSTRAINT burden_estimate_model_run_fkey,
-  DROP CONSTRAINT burden_estimate_burden_outcome_fkey;
+-- change data type
+ALTER TABLE burden_estimate DISABLE TRIGGER ALL;
 
 -- set data types to small ints
 ALTER TABLE burden_estimate
-  ALTER COLUMN year SET DATA TYPE SMALLINT,
-  ALTER COLUMN burden_outcome SET DATA TYPE SMALLINT;
+  ALTER COLUMN year SET DATA TYPE SMALLINT;
 
-ALTER TABLE burden_estimate
-  ADD COLUMN year_small SMALLINT NULL,
-  ADD COLUMN burden_outcome_small SMALLINT NULL;
+ALTER TABLE burden_estimate ENABLE TRIGGER ALL;
 
-UPDATE TABLE burden_estimate
-  ADD COLUMN
-
-  ADD CONSTRAINT burden_estimate_burden_estimate_set_fkey
-FOREIGN KEY (burden_estimate_set) REFERENCES burden_estimate_set (id),
-
-  ADD CONSTRAINT burden_estimate_country
-FOREIGN KEY (country) REFERENCES country (id),
-
-  ADD CONSTRAINT burden_estimate_pkey
-PRIMARY KEY (id),
-
-  ADD CONSTRAINT burden_estimate_model_run_fkey
-FOREIGN KEY (model_run) REFERENCES model_run (internal_id),
-
-  ADD CONSTRAINT burden_estimate_burden_outcome_fkey
-FOREIGN KEY (burden_outcome) REFERENCES burden_outcome (id);
-
--- add constraints back
-ALTER TABLE burden_estimate
-  ADD CONSTRAINT burden_estimate_unique UNIQUE (
-  burden_estimate_set,
-  country,
-  year,
-  age,
-  burden_outcome
-)
+-- change via new column
+-- ALTER TABLE burden_estimate
+--   ADD COLUMN year_small SMALLINT NULL;
+--
+-- UPDATE burden_estimate
+-- SET year_small = year;
+--
+-- ALTER TABLE burden_estimate
+--   DROP COLUMN year,
+-- RENAME COLUMN year_small TO year;
+--
+-- ALTER TABLE burden_estimate ENABLE TRIGGER ALL;
