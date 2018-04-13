d89ab01fc7c1827ca4fd9aa2f31882d1835740ec
Alex Hill <alex.hill@gmail.com>
2018-04-13 12:09:24 +0100

    new migration that recreates table

--- migrations/sql/V2018.02.15.1019__AlterBurdenOutcomeIndex.sql
+++ migrations/sql/V2018.02.15.1019__AlterBurdenOutcomeIndex.sql
@@ -1,6 +1,5 @@
 -- this took 16 minutes on UAT
 -- disable triggers
-ALTER TABLE burden_estimate DISABLE TRIGGER ALL;
 ALTER TABLE impact_estimate_ingredient DISABLE TRIGGER ALL;
 ALTER TABLE burden_outcome DISABLE TRIGGER ALL;
 
@@ -12,10 +11,6 @@ ALTER TABLE burden_outcome
 ALTER TABLE impact_estimate_ingredient
   ALTER COLUMN burden_outcome TYPE SMALLINT;
 
-ALTER TABLE burden_estimate
-  ALTER COLUMN burden_outcome TYPE SMALLINT;
-
 -- reenable triggers
-ALTER TABLE burden_estimate ENABLE TRIGGER ALL;
 ALTER TABLE impact_estimate_ingredient ENABLE TRIGGER ALL;
 ALTER TABLE burden_outcome ENABLE TRIGGER ALL;
--- migrations/sql/V2018.02.15.1029__AlterBurdenEstimateYearDataType.sql
+++
@@ -1,9 +0,0 @@
--- this took 1 hour 16 mins on UAT
--- change data type
-ALTER TABLE burden_estimate DISABLE TRIGGER ALL;
-
--- set data types to small ints
-ALTER TABLE burden_estimate
-  ALTER COLUMN year SET DATA TYPE SMALLINT;
-
-ALTER TABLE burden_estimate ENABLE TRIGGER ALL;
