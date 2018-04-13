35038b000dd2cc7b528a1cd17ca6d6e4bd4047b9
Alex Hill <alex.hill@gmail.com>
2018-04-13 12:13:44 +0100

    remove country from burden_estimate_stochastic

--- migrations/sql-annex/V2018.02.15.1500__RecreateBurdenEstimateStochasticWithSmallIntFields.sql
+++ migrations/sql-annex/V2018.02.15.1500__RecreateBurdenEstimateStochasticWithSmallIntFields.sql
@@ -3,8 +3,7 @@ DROP TABLE burden_estimate_stochastic;
 CREATE TABLE burden_estimate_stochastic (
   id BIGSERIAL NOT NULL,
   burden_estimate_set integer NOT NULL,
-  country text NOT NULL,
-  country_nid SMALLINT NULL,
+  country SMALLINT NULL,
   year SMALLINT NOT NULL,
   burden_outcome SMALLINT NOT NULL,
   value numeric,
@@ -17,7 +16,7 @@ CREATE INDEX ON burden_estimate_stochastic (burden_estimate_set);
 ALTER TABLE burden_estimate_stochastic
   ADD CONSTRAINT burden_estimate_stochastic_unique UNIQUE (
   burden_estimate_set,
-  country_nid,
+  country,
   year,
   age,
   burden_outcome) DEFERRABLE INITIALLY DEFERRED;
