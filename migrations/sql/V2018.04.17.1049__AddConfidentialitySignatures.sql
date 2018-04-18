0b6565f2dacea15850a489805a4e40a3fda2719e
Alex Hill <alex.hill@gmail.com>
2018-04-18 13:40:41 +0100

    rname to more generic

--- migrations/sql/V2018.04.17.1049__AddConfidentialitySignatures.sql
+++ migrations/sql/V2018.04.17.1049__AddConfidentialitySignatures.sql
@@ -1,15 +1,15 @@
-CREATE TABLE confidentiality_agreement
+CREATE TABLE legal_agreement
 (
 	name TEXT PRIMARY KEY,
 	description TEXT NOT NULL
 );
 
-CREATE TABLE confidentiality_agreement_signature
+CREATE TABLE user_legal_agreement
 (
-	confidentiality_agreement TEXT REFERENCES confidentiality_agreement (name),
+	legal_agreement TEXT REFERENCES legal_agreement (name),
 	username TEXT REFERENCES app_user (username),
 	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO confidentiality_agreement
+INSERT INTO legal_agreement
 VALUES ('rfp-applicants-04-18', 'RfP applicants confidentiality agreement April 2018')
