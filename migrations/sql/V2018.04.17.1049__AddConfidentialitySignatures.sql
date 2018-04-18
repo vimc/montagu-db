9fcdbaeb25dd932d512e11c1deacb6108af305e2
Alex Hill <alex.hill@gmail.com>
2018-04-18 11:58:03 +0100

    use extra table as lookup

--- migrations/sql/V2018.04.17.1049__AddConfidentialitySignatures.sql
+++ migrations/sql/V2018.04.17.1049__AddConfidentialitySignatures.sql
@@ -1,5 +1,15 @@
 CREATE TABLE confidentiality_agreement
 (
+	name TEXT PRIMARY KEY,
+	description TEXT NOT NULL
+);
+
+CREATE TABLE confidentiality_agreement_signature
+(
+	confidentiality_agreement TEXT REFERENCES confidentiality_agreement (name),
 	username TEXT REFERENCES app_user (username),
 	date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO confidentiality_agreement
+VALUES ('rfp-applicants-04-18', 'RfP applicants confidentiality agreement April 2018')
