aceac53cc004ba4626b067039bf972917d4229a0
Martin Eden <martin.s.eden@pm.me>
2018-04-23 13:58:56 +0100

    Add annex migration.

--- migrations/sql/V2018.04.23.1101__BurdenEstimate_RemoveArtificialKey.sql
+++ migrations/sql/V2018.04.23.1101__BurdenEstimate_RemoveArtificialKey.sql
@@ -1,8 +1,8 @@
 ALTER TABLE burden_estimate
   DROP COLUMN id,
+  DROP CONSTRAINT burden_estimate_unique,
   ADD PRIMARY KEY (burden_estimate_set,
-                   country,
-                   year,
-                   age,
-                   burden_outcome),
-  DROP CONSTRAINT burden_estimate_unique;
+                      country,
+                      year,
+                      age,
+                      burden_outcome);
