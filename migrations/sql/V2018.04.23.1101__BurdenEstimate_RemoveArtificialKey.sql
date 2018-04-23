21cdda9665da15ee76c2612d26c7588eb9703e98
Martin Eden <martin.s.eden@pm.me>
2018-04-23 15:49:20 +0100

    Don't have a primary key on burden_estimate

--- migrations/sql/V2018.04.23.1101__BurdenEstimate_RemoveArtificialKey.sql
+++ migrations/sql/V2018.04.23.1101__BurdenEstimate_RemoveArtificialKey.sql
@@ -1,11 +1,5 @@
 ALTER TABLE burden_estimate
-  DROP COLUMN id,
-  DROP CONSTRAINT burden_estimate_unique,
-  ADD PRIMARY KEY (burden_estimate_set,
-                      country,
-                      year,
-                      age,
-                      burden_outcome);
+  DROP COLUMN id;
 
 ALTER FOREIGN TABLE burden_estimate_stochastic
   DROP COLUMN id;
