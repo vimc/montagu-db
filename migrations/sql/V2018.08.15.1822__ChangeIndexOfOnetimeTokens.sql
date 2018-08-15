26b5dbf45219a624fa0befa1de9ed0f1e21f7e5a
Alex Hill <alex.hill@gmail.com>
2018-08-15 18:37:03 +0100

    use basic serial primary key

--- migrations/sql/V2018.08.15.1822__ChangeIndexOfOnetimeTokens.sql
+++ migrations/sql/V2018.08.15.1822__ChangeIndexOfOnetimeTokens.sql
@@ -1,3 +1,3 @@
-CREATE UNIQUE INDEX CONCURRENTLY onetime_token_pkey_temp ON onetime_token (md5(token));
-ALTER TABLE onetime_token DROP CONSTRAINT onetime_token_pkey,
-	ADD CONSTRAINT onetime_token_pkey PRIMARY KEY USING INDEX onetime_token_pkey_temp;
+ALTER TABLE onetime_token DROP CONSTRAINT onetime_token_pkey;
+ALTER TABLE onetime_token ADD COLUMN id SERIAL PRIMARY KEY;
+
