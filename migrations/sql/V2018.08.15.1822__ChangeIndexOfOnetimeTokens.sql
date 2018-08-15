75ce3108fdecaa489d27d7e366b5f81df1bde49a
Alex Hill <alex.hill@gmail.com>
2018-08-15 18:34:31 +0100

    recreate primary key using m5 hash

--- migrations/sql/V2018.08.15.1822__ChangeIndexOfOnetimeTokens.sql
+++ migrations/sql/V2018.08.15.1822__ChangeIndexOfOnetimeTokens.sql
@@ -1,1 +1,3 @@
-ALTER TABLE onetime_token DROP CONSTRAINT onetime_token_pkey;
+CREATE UNIQUE INDEX CONCURRENTLY onetime_token_pkey_temp ON onetime_token (md5(token));
+ALTER TABLE onetime_token DROP CONSTRAINT onetime_token_pkey,
+	ADD CONSTRAINT onetime_token_pkey PRIMARY KEY USING INDEX onetime_token_pkey_temp;
