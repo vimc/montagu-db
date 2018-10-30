412ab0edadd4b5f24d5d7f9600cd2cf59350c6db
Alex Hill <alex.hill@gmail.com>
2018-10-30 18:30:21 +0000

    wrap statement

--- migrations/sql/V2018.10.30.1809__UpdateRolesAndPermissions.sql
+++ migrations/sql/V2018.10.30.1809__UpdateRolesAndPermissions.sql
@@ -3,23 +3,25 @@ DELETE FROM role_permission WHERE role = 1 AND permission IN
 'modelling-groups.read',
 'users.read');
 
-declare admin int;
-SELECT id FROM role INTO admin WHERE name = 'admin';
+DO $$
+  declare admin int;
+  SELECT id FROM role INTO admin WHERE name = 'admin';
 
-declare developer int;
-SELECT id FROM role INTO developer WHERE name = 'developer';
+  declare developer int;
+  SELECT id FROM role INTO developer WHERE name = 'developer';
 
-declare member int;
-SELECT id FROM role INTO member WHERE name = 'member';
+  declare member int;
+  SELECT id FROM role INTO member WHERE name = 'member';
 
-INSERT INTO role_permission VALUES (admin, 'estimates.read')
-INSERT INTO role_permission VALUES (admin, 'modelling-groups.read')
-INSERT INTO role_permission VALUES (admin, 'users.read')
+  INSERT INTO role_permission VALUES (admin, 'estimates.read')
+  INSERT INTO role_permission VALUES (admin, 'modelling-groups.read')
+  INSERT INTO role_permission VALUES (admin, 'users.read')
 
-INSERT INTO role_permission VALUES (developer, 'estimates.read')
-INSERT INTO role_permission VALUES (developer, 'modelling-groups.read')
-INSERT INTO role_permission VALUES (developer, 'users.read')
+  INSERT INTO role_permission VALUES (developer, 'estimates.read')
+  INSERT INTO role_permission VALUES (developer, 'modelling-groups.read')
+  INSERT INTO role_permission VALUES (developer, 'users.read')
 
-INSERT INTO role_permission VALUES (member, 'estimates.read')
-INSERT INTO role_permission VALUES (member, 'modelling-groups.read')
-INSERT INTO role_permission VALUES (member, 'users.read')
+  INSERT INTO role_permission VALUES (member, 'estimates.read')
+  INSERT INTO role_permission VALUES (member, 'modelling-groups.read')
+  INSERT INTO role_permission VALUES (member, 'users.read')
+end $$;
