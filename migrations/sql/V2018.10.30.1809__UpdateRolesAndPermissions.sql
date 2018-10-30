33e505f199d1b3881d0126dc2dad81c9e64c6cd7
Alex Hill <alex.hill@gmail.com>
2018-10-30 18:25:23 +0000

    add permissions to other roles

--- migrations/sql/V2018.10.30.1809__UpdateRolesAndPermissions.sql
+++ migrations/sql/V2018.10.30.1809__UpdateRolesAndPermissions.sql
@@ -2,3 +2,24 @@ DELETE FROM role_permisison WHERE role = 1 AND permission IN
 ('estimates.read',
 'modelling-groups.read',
 'users.read');
+
+declare admin int;
+SELECT id FROM role INTO admin WHERE name = 'admin';
+
+declare developer int;
+SELECT id FROM role INTO developer WHERE name = 'developer';
+
+declare member int;
+SELECT id FROM role INTO member WHERE name = 'member';
+
+INSERT INTO role_permission VALUES (admin, 'estimates.read')
+INSERT INTO role_permission VALUES (admin, 'modelling-groups.read')
+INSERT INTO role_permission VALUES (admin, 'users.read')
+
+INSERT INTO role_permission VALUES (developer, 'estimates.read')
+INSERT INTO role_permission VALUES (developer, 'modelling-groups.read')
+INSERT INTO role_permission VALUES (developer, 'users.read')
+
+INSERT INTO role_permission VALUES (member, 'estimates.read')
+INSERT INTO role_permission VALUES (member, 'modelling-groups.read')
+INSERT INTO role_permission VALUES (member, 'users.read')
