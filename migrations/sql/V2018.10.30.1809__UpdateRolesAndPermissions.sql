7df38335d9465479869237378bc7c80ed3710326
Alex Hill <alex.hill@gmail.com>
2018-10-31 16:42:47 +0000

    missing begin statement

--- migrations/sql/V2018.10.30.1809__UpdateRolesAndPermissions.sql
+++ migrations/sql/V2018.10.30.1809__UpdateRolesAndPermissions.sql
@@ -1,27 +1,29 @@
-DELETE FROM role_permission WHERE role = 1 AND permission IN
-('estimates.read',
-'modelling-groups.read',
-'users.read');
+DELETE FROM role_permission
+WHERE role = 1
+AND permission IN ('estimates.read',
+                           'modelling-groups.read',
+                           'users.read');
+
 
 DO $$
   declare admin int;
-  SELECT id INTO admin FROM role WHERE name = 'admin';
-
   declare developer int;
-  SELECT id INTO developer FROM role WHERE name = 'developer';
-
   declare member int;
+begin
+  SELECT id INTO admin FROM role WHERE name = 'admin';
+  SELECT id INTO developer FROM role WHERE name = 'developer';
   SELECT id INTO member FROM role WHERE name = 'member';
 
-  INSERT INTO role_permission VALUES (admin, 'estimates.read')
-  INSERT INTO role_permission VALUES (admin, 'modelling-groups.read')
-  INSERT INTO role_permission VALUES (admin, 'users.read')
+  INSERT INTO role_permission VALUES (admin, 'estimates.read');
+  INSERT INTO role_permission VALUES (admin, 'modelling-groups.read');
+  INSERT INTO role_permission VALUES (admin, 'users.read');
 
-  INSERT INTO role_permission VALUES (developer, 'estimates.read')
-  INSERT INTO role_permission VALUES (developer, 'modelling-groups.read')
-  INSERT INTO role_permission VALUES (developer, 'users.read')
+  INSERT INTO role_permission VALUES (developer, 'estimates.read');
+  INSERT INTO role_permission VALUES (developer, 'modelling-groups.read');
+  INSERT INTO role_permission VALUES (developer, 'users.read');
 
-  INSERT INTO role_permission VALUES (member, 'estimates.read')
-  INSERT INTO role_permission VALUES (member, 'modelling-groups.read')
-  INSERT INTO role_permission VALUES (member, 'users.read')
+  INSERT INTO role_permission VALUES (member, 'estimates.read');
+  INSERT INTO role_permission VALUES (member, 'modelling-groups.read');
+  INSERT INTO role_permission VALUES (member, 'users.read');
+
 end $$;
