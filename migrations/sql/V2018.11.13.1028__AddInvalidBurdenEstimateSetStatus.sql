9083a47cfaab62aaf17e0e3d0c399943282d7417
Alex Hill <alex.hill@gmail.com>
2018-11-13 11:13:36 +0000

    fix migration and rename old migration

--- migrations/sql/V2018.11.13.1028__AddInvalidBurdenEstimateSetStatus.sql
+++ migrations/sql/V2018.11.13.1028__AddInvalidBurdenEstimateSetStatus.sql
@@ -1,5 +1,3 @@
 COMMENT ON COLUMN burden_estimate_set_status.code IS 'Valid values: {empty, partial, complete, invalid}';
 
-INSERT INTO burden_estimate_set_status VALUES ('invalid', 'Rows do not match expecations');
-INSERT INTO burden_estimate_set_status VALUES ('partial', 'Some but not all burden estimates added');
-INSERT INTO burden_estimate_set_status VALUES ('complete', 'Closed to further estimates');
+INSERT INTO burden_estimate_set_status VALUES ('invalid', 'Rows do not match expectations');
