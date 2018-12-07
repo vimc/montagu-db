be8460626531c0a7829eeaf46c5f99a3c19aea45
Alex Hill <alex.hill@gmail.com>
2018-12-07 11:23:38 +0000

    new migration

---
+++ migrations/sql/V2018.12.07.1121__AddGaviSupportLevels.sql
@@ -0,0 +1,14 @@
+INSERT INTO gavi_support_level (id, name)
+VALUES ('high', 'Assuming 10% above OP coverage')
+ON CONFLICT DO NOTHING;;
+INSERT INTO gavi_support_level (id, name)
+ VALUES ('low', 'Assuming 10% below OP coverage')
+ON CONFLICT DO NOTHING;;
+INSERT INTO gavi_support_level (id, name)
+VALUES ('bestcase', 'BMGF best case scenario: assuming best coverage for future(90%/95%/histotical highest)')
+ON CONFLICT DO NOTHING;;
+
+UPDATE gavi_support_level
+SET name = 'BMGF best case scenario: assuming best coverage for future(90%/95%/historical highest)'
+WHERE id = 'bestcase';
+
