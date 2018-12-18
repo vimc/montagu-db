c870ef6fc0f2e37997d9940829198b9f5e2a8310
Xiang Li <x.li@imperial.ac.uk>
2018-12-18 09:19:11 +0000

    commit changes

--- migrations/sql/V2018.11.20.1106__AddModelMeta.sql
+++ migrations/sql/V2018.11.20.1106__AddModelMeta.sql
@@ -16,15 +16,15 @@ CREATE TABLE model_version_meta (
   id SERIAL,
   model_version INTEGER NOT NULL,
   is_dynamic BOOLEAN NOT NULL, 
-  code TEXT,
-  dalys BOOLEAN NOT NULL,
+  language TEXT,
+  computes_dalys BOOLEAN NOT NULL,
   gender INTEGER NOT NULL,
   burden_min_age INTEGER NOT NULL,
   burden_max_age INTEGER NOT NULL,
-  cohort_first INTEGER, --- nullable, NA means 2000
-  cohort_last INTEGER, --- nullable, NA means min(2100, age_max+2030)
-  year_min INTEGER, --- nullable, NA means 2000
-  year_max INTEGER, --- nullable, NA means 2100
+  cohort_min INTEGER NOT NULL,
+  cohort_max INTEGER NOT NULL,
+  year_min INTEGER NOT NULL,
+  year_max INTEGER NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (model_version) REFERENCES model_version(id),
   FOREIGN KEY (gender) REFERENCES gender(id)
@@ -37,7 +37,6 @@ CREATE TABLE model_version_vaccine (
   model_version INTEGER NOT NULL,
   vaccine TEXT NOT NULL, 
   activity_type TEXT NOT NULL, 
-  delivery_order INTEGER NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (model_version) REFERENCES model_version(id),
   FOREIGN KEY (vaccine) REFERENCES vaccine(id),
@@ -49,14 +48,13 @@ COMMENT ON TABLE model_version_vaccine IS
 CREATE TABLE model_version_country (
   id SERIAL,
   model_version INTEGER NOT NULL,
-  country TEXT NOT NULL, 
-  is_pine BOOLEAN NOT NULL, 
+  pine_country TEXT NOT NULL, 
   PRIMARY KEY (id),
   FOREIGN KEY (model_version) REFERENCES model_version(id),
-  FOREIGN KEY (country) REFERENCES country(id)
+  FOREIGN KEY (pine_country) REFERENCES country(id)
 );
 COMMENT ON TABLE model_version_country IS
-  'Specify which countries are evaluated in each model, and which countries are considered in small-scale model run.';
+  'Specify which pine countries are considered in small-scale model run.';
 
 CREATE TABLE model_version_outcome (
   id SERIAL,
