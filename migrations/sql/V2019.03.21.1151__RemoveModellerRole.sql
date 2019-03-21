e7a91a4d0cd28af9b16f210d55f71c5e0a2e4dd1
Alex <alex.hill@gmail.com>
2019-03-21 15:14:45 +0000

    correct command

--- migrations/sql/V2019.03.21.1151__RemoveModellerRole.sql
+++ migrations/sql/V2019.03.21.1151__RemoveModellerRole.sql
@@ -6,6 +6,6 @@ begin
 
   DELETE FROM user_group_role where role = role_id;
   DELETE FROM role_permission where role = role_id;
-  DELETE FROM role where role = role_id;
+  DELETE FROM role where id = role_id;
 
 end $$;
