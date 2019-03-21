10453a5a1bb7dafe43e2af516b87cb406efd8b96
Alex <alex.hill@gmail.com>
2019-03-21 15:07:58 +0000

    no, its definitely id

--- migrations/sql/V2019.03.21.1151__RemoveModellerRole.sql
+++ migrations/sql/V2019.03.21.1151__RemoveModellerRole.sql
@@ -1,7 +1,8 @@
 DO $$
 declare role_id int;
 begin
-  SELECT name INTO role_id FROM role WHERE name = 'modeller';
+
+  SELECT id INTO role_id FROM role WHERE name = 'modeller';
 
   DELETE FROM user_group_role where role = role_id;
   DELETE FROM role_permission where role = role_id;
