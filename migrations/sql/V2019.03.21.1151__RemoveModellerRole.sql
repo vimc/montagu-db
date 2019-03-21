95ccabddb6226c2330490a5ec864e403e7dbe6e4
Alex <alex.hill@gmail.com>
2019-03-21 12:13:17 +0000

    role has name not id

--- migrations/sql/V2019.03.21.1151__RemoveModellerRole.sql
+++ migrations/sql/V2019.03.21.1151__RemoveModellerRole.sql
@@ -1,10 +1,10 @@
 DO $$
 declare role_id int;
 begin
-  SELECT id INTO role_id FROM role WHERE name = 'modeller';
+  SELECT name INTO role_id FROM role WHERE name = 'modeller';
 
   DELETE FROM user_group_role where role = role_id;
   DELETE FROM role_permission where role = role_id;
   DELETE FROM role where role = role_id;
-  
+
 end $$;
