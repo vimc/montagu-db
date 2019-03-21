6e8d549a3e8f7889f122d544c3b6a5a09a6f6e0f
Alex <alex.hill@gmail.com>
2019-03-21 11:57:40 +0000

    add migration to remove modeller role

---
+++ migrations/sql/V2019.03.21.1151__RemoveModellerRole.sql
@@ -0,0 +1,9 @@
+DO $$
+declare role_id int;
+begin
+  SELECT id INTO role_id FROM role WHERE name = 'modeller';
+
+  DELETE FROM user_group_role where role = role_id
+  DELETE FROM role_permission where role = role_id
+  DELETE FROM role where role = role_id
+end $$;
