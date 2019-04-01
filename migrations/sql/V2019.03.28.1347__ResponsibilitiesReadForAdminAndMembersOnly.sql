3cc9c0c2d2481f1072ccb7b5f0d62b7cf99c4f1d
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-04-01 09:25:47 +0100

    added missing semicolons

--- migrations/sql/V2019.03.28.1347__ResponsibilitiesReadForAdminAndMembersOnly.sql
+++ migrations/sql/V2019.03.28.1347__ResponsibilitiesReadForAdminAndMembersOnly.sql
@@ -5,11 +5,11 @@ DO $$
   DECLARE admin_role INT;
   DECLARE developer_role INT;
 BEGIN
-  SELECT id INTO user_role FROM role WHERE name = 'user'
-  SELECT id INTO member_role FROM role WHERE name = 'member'
-  SELECT id INTO funder_role FROM role WHERE name = 'funder'
-  SELECT id INTO admin_role FROM role WHERE name = 'admin'
-  SELECT id INTO developer_role FROM role WHERE name = 'developer'
+  SELECT id INTO user_role FROM role WHERE name = 'user';
+  SELECT id INTO member_role FROM role WHERE name = 'member';
+  SELECT id INTO funder_role FROM role WHERE name = 'funder';
+  SELECT id INTO admin_role FROM role WHERE name = 'admin';
+  SELECT id INTO developer_role FROM role WHERE name = 'developer';
 
   DELETE FROM role_permission WHERE role=user_role AND permission='responsibilities.read';
 
