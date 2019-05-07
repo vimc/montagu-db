56402342378985f35e10a26983e8dda639b55348
Robert Ashton <r.ashton@imperial.ac.uk>
2019-05-07 16:54:59 +0100

    Create dettl import log table

---
+++ migrations/sql/V2019.05.03.0830__CreateDettlImportLog.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+CREATE TABLE dettl_import_log (
+  name       TEXT PRIMARY KEY,
+  date       TIMESTAMP WITH TIME ZONE,
+  comment    TEXT,
+  git_user   TEXT,
+  git_email  TEXT,
+  git_branch TEXT,
+  git_hash   CHARACTER(40)
+);
+
+COMMIT;
