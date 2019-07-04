e713e66dc2be35f84cf9d31a98f455fc3cf34b1f
Emma Russell <EmmaRussellKilburn@gmail.com>
2019-07-04 16:05:43 +0100

    Added migration for missing model metadata

---
+++ migrations/sql/V2019.07.04.1441__AddModelMetadata.sql
@@ -0,0 +1,11 @@
+ALTER TABLE model
+    ADD COLUMN gender_specific boolean DEFAULT FALSE,
+    ADD COLUMN gender integer DEFAULT NULL,
+    ADD FOREIGN KEY (gender) REFERENCES gender (id)
+    ;
+
+ALTER TABLE model_version
+    ADD COLUMN code text DEFAULT NULL,
+    ADD COLUMN is_dynamic boolean DEFAULT FALSE,
+    ADD COLUMN max_countries integer
+    ;
