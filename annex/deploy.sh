ed71171909925c3ac2e479293c27f0554727f75c
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2020-02-12 11:24:47 +0000

    Only need the password on initial deploy

--- annex/deploy.sh
+++ annex/deploy.sh
@@ -11,9 +11,20 @@ MONTAGU_REGISTRY=docker.montagu.dide.ic.ac.uk:5000
 
 ANNEX_IMAGE=${MONTAGU_REGISTRY}/${ANNEX_IMAGE_NAME}:${ANNEX_IMAGE_VERSION}
 
-export VAULT_ADDR=https://support.montagu.dide.ic.ac.uk:8200
-vault login -method=github
-ANNEX_VIMC_PASSWORD=$(vault read -field=password /secret/annex/users/vimc)
+if docker volume inspect $ANNEX_VOLUME_NAME > /dev/null 2>&1; then
+    echo "montagu db annex volume already exists"
+    INITIAL_DEPLOY=0
+else
+    echo "Fetching annex password"
+    export VAULT_ADDR=https://support.montagu.dide.ic.ac.uk:8200
+    vault login -method=github
+    ANNEX_VIMC_PASSWORD=$(vault read -field=password /secret/annex/users/vimc)
+
+    echo "Creating montagu db annex volume"
+    INITIAL_DEPLOY=1
+    docker volume create $ANNEX_VOLUME_NAME
+fi
+
 
 docker pull $ANNEX_IMAGE
 
@@ -21,15 +32,6 @@ if docker inspect -f '{{.State.Running}}' $ANNEX_CONTAINER_NAME > /dev/null; the
     echo "montagu db annex already exists: stopping"
     docker stop $ANNEX_CONTAINER_NAME
     docker rm $ANNEX_CONTAINER_NAME
-fi
-
-if docker volume inspect $ANNEX_VOLUME_NAME > /dev/null 2>&1; then
-    echo "montagu db annex volume already exists"
-    INITIAL_DEPLOY=0
-else
-    echo "Creating montagu db annex volume"
-    INITIAL_DEPLOY=1
-    docker volume create $ANNEX_VOLUME_NAME
 fi
 
 docker run -d \
