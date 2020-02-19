17e0eae560f6947c5508ee338f5126633e20e998
Alex <alex.hill@gmail.com>
2020-02-19 17:30:32 +0000

    and reports.run

--- migrations/sql/V2020.02.19.1135__RemoveReportPermissions.sql
+++ migrations/sql/V2020.02.19.1135__RemoveReportPermissions.sql
@@ -6,8 +6,10 @@ begin
     SELECT id INTO reviewer_id from role where name = 'reports.reviewer';
     DELETE FROM role_permission where permission = 'reports.read';
     DELETE FROM role_permission where permission = 'reports.review';
+    DELETE FROM role_permission where permission = 'reports.run';
     DELETE FROM permission where name = 'reports.read';
     DELETE FROM permission where name = 'reports.review';
+    DELETE FROM permission where name = 'reports.run';
     DELETE FROM user_group_role where role = reader_id;
     DELETE FROM user_group_role where role = reviewer_id;
     DELETE FROM role where name = 'reports.reader';
