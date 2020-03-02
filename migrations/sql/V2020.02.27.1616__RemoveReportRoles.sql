e03e7a11891effc41c94678c8095b07a21069146
Alex <alex.hill@gmail.com>
2020-03-02 16:45:39 +0000

    fix query

--- migrations/sql/V2020.02.27.1616__RemoveReportRoles.sql
+++ migrations/sql/V2020.02.27.1616__RemoveReportRoles.sql
@@ -4,8 +4,8 @@ DO $$
     declare scoped_reader_id int;
     declare reviewer_id int;
 begin
-    SELECT id INTO reader_id from role where name = 'reports-reader' is null;
-    SELECT id INTO scoped_reader_id from role where name = 'reports-reader' is not null;
+    SELECT id INTO reader_id from role where name = 'reports-reader' and scope_prefix is null;
+    SELECT id INTO scoped_reader_id from role where name = 'reports-reader' and scope_prefix is not null;
     SELECT id INTO reviewer_id from role where name = 'reports-reviewer';
     DELETE FROM user_group_role where role = reader_id;
     DELETE FROM user_group_role where role = scoped_reader_id;
