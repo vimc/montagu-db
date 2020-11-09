2bbf49ace9f292dc8cacbcc709fe44b023fca2ad
Alex <alex.hill@gmail.com>
2020-11-09 17:42:24 +0000

    allow for 1-many mapping

--- migrations/sql/V2020.11.09.1222__AddCoverageSetMetadata.sql
+++ migrations/sql/V2020.11.09.1222__AddCoverageSetMetadata.sql
@@ -1,6 +1,6 @@
 CREATE TABLE coverage_set_metadata
 (
-    "id"          INTEGER   NOT NULL,
+    "id"          SERIAL,
     "uploaded_by" TEXT      NOT NULL,
     "uploaded_on" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "filename"    TEXT      NULL,
@@ -8,8 +8,17 @@ CREATE TABLE coverage_set_metadata
     PRIMARY KEY ("id")
 );
 
+CREATE TABLE coverage_set_coverage_set_metadata
+(
+    "coverage_set"          INTEGER   NOT NULL,
+    "coverage_set_metadata" INTEGER      NOT NULL
+);
+
 ALTER TABLE "coverage_set_metadata"
     ADD FOREIGN KEY ("uploaded_by") REFERENCES "app_user" ("username");
 
-ALTER TABLE "coverage_set_metadata"
-    ADD FOREIGN KEY ("id") REFERENCES "coverage_set" ("id");
+ALTER TABLE "coverage_set_coverage_set_metadata"
+    ADD FOREIGN KEY ("coverage_set") REFERENCES "coverage_set" ("id");
+
+ALTER TABLE "coverage_set_coverage_set_metadata"
+    ADD FOREIGN KEY ("coverage_set_metadata") REFERENCES "coverage_set_metadata" ("id");
