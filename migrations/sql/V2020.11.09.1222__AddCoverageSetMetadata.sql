8df27321c4ca76e671172aa4f79a793e5524d974
Alex <alex.hill@gmail.com>
2020-11-10 09:57:52 +0000

    actual 1-many relationship

--- migrations/sql/V2020.11.09.1222__AddCoverageSetMetadata.sql
+++ migrations/sql/V2020.11.09.1222__AddCoverageSetMetadata.sql
@@ -1,24 +1,12 @@
-CREATE TABLE coverage_set_metadata
+CREATE TABLE coverage_set_upload_metadata
 (
     "id"          SERIAL,
-    "uploaded_by" TEXT      NOT NULL,
+    "uploaded_by" TEXT      NOT NULL REFERENCES app_user (username),
     "uploaded_on" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "filename"    TEXT      NULL,
     "description" TEXT      NOT NULL,
     PRIMARY KEY ("id")
 );
 
-CREATE TABLE coverage_set_coverage_set_metadata
-(
-    "coverage_set"          INTEGER   NOT NULL,
-    "coverage_set_metadata" INTEGER      NOT NULL
-);
-
-ALTER TABLE "coverage_set_metadata"
-    ADD FOREIGN KEY ("uploaded_by") REFERENCES "app_user" ("username");
-
-ALTER TABLE "coverage_set_coverage_set_metadata"
-    ADD FOREIGN KEY ("coverage_set") REFERENCES "coverage_set" ("id");
-
-ALTER TABLE "coverage_set_coverage_set_metadata"
-    ADD FOREIGN KEY ("coverage_set_metadata") REFERENCES "coverage_set_metadata" ("id");
+ALTER TABLE coverage_set
+    ADD COLUMN coverage_set_upload_metadata INTEGER NULL REFERENCES coverage_set_upload_metadata (id);
