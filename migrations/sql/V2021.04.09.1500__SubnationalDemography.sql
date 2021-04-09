f1ed970436caf8087fe1f17801791ff34a89c2cf
Wes Hinsley <w.hinsley@imperial.ac.uk>
2021-04-09 17:32:20 +0100

    Better names

--- migrations/sql/V2021.04.09.1500__SubnationalDemography.sql
+++ migrations/sql/V2021.04.09.1500__SubnationalDemography.sql
@@ -1,16 +1,17 @@
-CREATE TABLE "subnational_region" (
+CREATE TABLE "region_subnational" (
 "id" SERIAL NOT NULL ,
 "country" INTEGER NOT NULL ,
 "name" TEXT NOT NULL ,
 "admin_level" INTEGER NOT NULL ,
-"parent_id" INTEGER,
+"parent_id" INTEGER NULL,
 PRIMARY KEY ("id") ,
 UNIQUE ("id", "name")
 );
 
-ALTER TABLE "subnational_region" ADD FOREIGN KEY ("country") REFERENCES "country" ("nid");
+ALTER TABLE "region_subnational" ADD FOREIGN KEY ("country") REFERENCES "country" ("nid");
+ALTER TABLE "parent_id" REFERENCES region_subnational ("id");
 
-CREATE TABLE "subnational_demographic_statistic" (
+CREATE TABLE "demographic_subnational_statistic" (
 "id" SERIAL NOT NULL ,
 "region" INTEGER NOT NULL ,
 "age_from" INTEGER NOT NULL ,
@@ -21,6 +22,6 @@ CREATE TABLE "subnational_demographic_statistic" (
 "demographic_dataset" INTEGER NOT NULL
 );
 
-ALTER TABLE "subnational_demographic_statistic" ADD FOREIGN KEY ("region") REFERENCES "subnational_region" ("id");
-ALTER TABLE "subnational_demographic_statistic" ADD FOREIGN KEY ("gender") REFERENCES "gender" ("id");
-ALTER TABLE "subnational_demographic_statistic" ADD FOREIGN KEY ("demographic_dataset") REFERENCES "demographic_dataset" ("id");
+ALTER TABLE "demographic_subnational_statistic" ADD FOREIGN KEY ("region") REFERENCES "region_subnational" ("id");
+ALTER TABLE "demographic_subnational_statistic" ADD FOREIGN KEY ("gender") REFERENCES "gender" ("id");
+ALTER TABLE "demographic_subnational_statistic" ADD FOREIGN KEY ("demographic_dataset") REFERENCES "demographic_dataset" ("id");
