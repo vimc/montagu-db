1aa916fcd540375bf42d1a348af3cbef6ae30d57
Robert Ashton <r.ashton@imperial.ac.uk>
2021-07-27 10:32:27 +0100

    Add additional burden outcomes

---
+++ migrations/sql/V2021.07.27.1030_AddBurdenOutcomes.sql
@@ -0,0 +1,4 @@
+INSERT INTO burden_outcome (code, name, proportion) VALUES ('cases_cwyx', 'Cases attributable to Meningitis serogroups C,W,X and Y', FALSE);
+INSERT INTO burden_outcome (code, name, proportion) VALUES ('deaths_cwyx', 'Deaths attributable to Meningitis serogroups C,W,X and Y', FALSE);
+INSERT INTO burden_outcome (code, name, proportion) VALUES ('dalys_cwyx', 'Dalys attributable to Meningitis serogroups C,W,X and Y', FALSE);
+
