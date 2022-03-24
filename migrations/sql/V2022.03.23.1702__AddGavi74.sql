4c082f13d1639aed5bc1901e050f351437147cb6
Xiang Li <x.li@imperial.ac.uk>
2022-03-24 10:11:10 +0000

    set default in correct way

--- migrations/sql/V2022.03.23.1702__AddGavi74.sql
+++ migrations/sql/V2022.03.23.1702__AddGavi74.sql
@@ -1,4 +1,6 @@
-ALTER TABLE country_metadata
-    ADD COLUMN gavi74 BOOLEAN default FALSE,
-    ALTER COLUMN gavi74 SET NOT NULL;
-
+ALTER TABLE country_metadata 
+	ADD COLUMN gavi74 BOOLEAN;
+UPDATE country_metadata 
+	SET gavi74 = FALSE;
+ALTER TABLE country_metadata 
+	ALTER COLUMN gavi74 SET NOT NULL;
