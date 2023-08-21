8a349fcee12c4407bcd4fad3e0336eafd8a7c56e
Xiang Li <x.li@imperial.ac.uk>
2023-08-21 13:02:25 +0100

    upload script

---
+++ migrations/sql/V2023.08.21.1254__AddPercentGaviSupportToCoverage.sql
@@ -0,0 +1,5 @@
+-- To carry out new impact calculations we need to indicate
+-- percentage of GAVI support in a given vaccination activity.
+ALTER TABLE coverage
+    ADD COLUMN percent_gavi_support DECIMAL DEFAULT NULL
+    ;
