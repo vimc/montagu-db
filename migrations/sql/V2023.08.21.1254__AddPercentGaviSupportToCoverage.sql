de3bc701fc717c821303f151ce46d5828b2f5d16
Xiang Li <x.li@imperial.ac.uk>
2023-08-21 14:59:56 +0100

    Update migrations/sql/V2023.08.21.1254__AddPercentGaviSupportToCoverage.sql
    Co-authored-by: Rich FitzJohn <r.fitzjohn@imperial.ac.uk>

--- migrations/sql/V2023.08.21.1254__AddPercentGaviSupportToCoverage.sql
+++ migrations/sql/V2023.08.21.1254__AddPercentGaviSupportToCoverage.sql
@@ -1,5 +1,5 @@
 -- To carry out new impact calculations we need to indicate
 -- percentage of GAVI support in a given vaccination activity.
 ALTER TABLE coverage
-    ADD COLUMN percent_gavi_support DECIMAL DEFAULT NULL
+    ADD COLUMN percent_gavi_support DOUBLE PRECISION DEFAULT NULL
     ;
